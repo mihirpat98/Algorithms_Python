005fe2d5cb69d5d9853d8e4efbe2aa7daa7c1fa1
Mihir Ravindra Patel <mihir98pat@gmail.com>
2022-03-02 09:43:39 -0500

    Add files via upload

---
+++ Binary_Search/Finding_Boundary_Bin_Search.py
@@ -0,0 +1,19 @@
+list1 = ['False', 'False','False','True','True']
+#Find the boundary where the boundary is the first true element index
+
+from typing import List
+
+def find_boundary(arr: List[bool]) -> int:
+    # WRITE YOUR BRILLIANT CODE HERE
+    l = 0
+    r = len(arr) -1
+    boundary_index =-1
+    while l<=r:
+        mid= (r+l)//2
+        
+        if arr[mid]:
+            r = mid-1
+            boundary_index = mid
+        else:
+            l = mid+1
+    return boundary_index
